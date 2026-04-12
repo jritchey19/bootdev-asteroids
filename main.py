8ad95b2b96712b579dfe58466c9b5e519de6b236
jritchey19 <jritchey19@yahoo.com>
2026-04-12 21:19:50 +0000

    added player to game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -10,6 +11,11 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x,y)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +23,8 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
