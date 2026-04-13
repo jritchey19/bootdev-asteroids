4f4213ed41c62a272e6d618d682e516ce14ef083
jritchey19 <jritchey19@yahoo.com>
2026-04-13 03:51:39 +0000

    added asteroid field

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,10 +13,20 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroidfield = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
     player = Player(x,y)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -24,7 +36,11 @@ def main():
 
         screen.fill("black")
 
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
